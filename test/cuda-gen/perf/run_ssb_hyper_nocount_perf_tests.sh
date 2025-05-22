0217fd352c34b9d76accb06d7999dfe94c16ae74
Chethan R <chethan.vasishta@gmail.com>
2025-05-22 13:40:14 +0530

    Add SSB performance tests for hyper and hyper-nocount

--- test/cuda-gen/perf/run_ssb_hyper_nocount_perf_tests.sh
+++ test/cuda-gen/perf/run_ssb_hyper_nocount_perf_tests.sh
@@ -23,7 +23,7 @@ fi
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Get the path of the parent directory
-TEST_DIR="$(dirname "$SCRIPT_DIR")"
+TEST_DIR="$(dirname $(dirname "$SCRIPT_DIR"))"
 REPO_DIR="$(dirname "$TEST_DIR")"
 
 SSB_DIR="$REPO_DIR/resources/sql/ssb"
@@ -37,7 +37,7 @@ fi
 
 # List of queries to run
 QUERIES=(11 12 13 21 22 23 31 32 33 34 41 42 43)
-# QUERIES=(33)
+# QUERIES=(11)
 
 pushd $SQL_PLAN_COMPILER_DIR/gpu-db/ssb
 MAKE_RUNTIME="make build-runtime CUCO_SRC_PATH=$CUCO_SRC_PATH"
@@ -45,16 +45,22 @@ echo $MAKE_RUNTIME
 $MAKE_RUNTIME
 popd
 
+OUTPUT_FILE=$SCRIPT_DIR/ssb-hyper-nocount-perf.csv
+echo "Output file: $OUTPUT_FILE"
+
+# Empty the output file
+echo -n "" > $OUTPUT_FILE
+
 # Iterate over the queries
 for QUERY in "${QUERIES[@]}"; do
   # First run the run-sql tool to generate CUDA and get reference output
-  RUN_SQL="$BUILD_DIR/run-sql $SSB_DIR/$QUERY.sql $SSB_DATA_DIR --gen-cuda-code-no-count --ssb"
-  OUTPUT_FILE="ssb-$QUERY-ref.csv"
-  echo $RUN_SQL
-  $RUN_SQL > $OUTPUT_FILE
+  COMPILE_SQL="$BUILD_DIR/run-sql $SSB_DIR/$QUERY.sql $SSB_DATA_DIR --gen-cuda-code-no-count --ssb --gen-kernel-timing"
+  echo $COMPILE_SQL
+  $COMPILE_SQL
+
+  NOCOUNT="$QUERY.nocount"
 
   # Now run the generated CUDA code
-  NOCOUNT="$QUERY.nocount"
   CP_CMD="cp output.cu $SQL_PLAN_COMPILER_DIR/gpu-db/ssb/q$NOCOUNT.codegen.cu"
   echo $CP_CMD
   $CP_CMD
@@ -67,40 +73,15 @@ for QUERY in "${QUERIES[@]}"; do
   echo $MAKE_QUERY
   $MAKE_QUERY
 
+  # Append the query number to the output file
+  echo "---" >> $OUTPUT_FILE
+  echo "ssb-q$NOCOUNT" >> $OUTPUT_FILE
+
   RUN_QUERY_CMD="build/dbruntime --data_dir $SSB_DATA_DIR/ --query_num $NOCOUNT"
   echo $RUN_QUERY_CMD
-  $RUN_QUERY_CMD > "cuda-ssb-$NOCOUNT.csv" 2> "cuda-ssb-$NOCOUNT.log"
+  $RUN_QUERY_CMD >> $OUTPUT_FILE
 
   cd -
 
-  PYTHON_CMD="python $SCRIPT_DIR/compare_tpch_outputs.py $OUTPUT_FILE $SQL_PLAN_COMPILER_DIR/gpu-db/ssb/cuda-ssb-$NOCOUNT.csv"
-  echo $PYTHON_CMD
-  $PYTHON_CMD
-
-  # If the comparison fails, add query to the failed list
-  if [ $? -ne 0 ]; then
-    echo "Query $QUERY failed"
-    FAILED_QUERIES+=($QUERY)
-  else
-    echo "Query $QUERY passed"
-    PASSED_QUERIES+=($QUERY)
-  fi
 done
 
-# Print the results
-# Print some new lines and then a seperator
-echo -e "\n"
-echo "=============================="
-echo "SSB TEST RESULTS"
-echo "=============================="
-echo "Passed queries: ${PASSED_QUERIES[@]}"
-echo "Failed queries: ${FAILED_QUERIES[@]}"
-echo -e "\n"
-if [ ${#FAILED_QUERIES[@]} -eq 0 ]; then
-  # Print "TEST PASSED" in green
-  echo -e "\033[0;32mTEST PASSED!\033[0m"
-else
-  # Print "TEST FAILED" in red
-  echo -e "\033[0;31mTEST FAILED!\033[0m"
-  exit 1
-fi
