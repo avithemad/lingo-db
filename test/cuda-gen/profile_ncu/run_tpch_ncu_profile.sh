ea3627dfebfd44c287cab923c62cc270dc4bc559
Chethan R <chethan.vasishta@gmail.com>
2025-06-15 10:24:46 +0530

    Making the test and profile scripts consider scale factor for tpch queries.

--- test/cuda-gen/profile_ncu/run_tpch_ncu_profile.sh
+++ test/cuda-gen/profile_ncu/run_tpch_ncu_profile.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 # The first argument is the directory where sql-plan-compiler is
-SQL_PLAN_COMPILER_DIR="$1"
-if [ -z "$SQL_PLAN_COMPILER_DIR" ]; then
-  echo "Usage: $0 <sql-plan-compiler-dir> <cuco-src-path>"
-  exit 1
-fi
+CUR_GPU="A6000" # TODO: Change this when you change GPUs
+# SQL_PLAN_COMPILER_DIR="$1"
+# if [ -z "$SQL_PLAN_COMPILER_DIR" ]; then
+#   echo "Usage: $0 <sql-plan-compiler-dir> <cuco-src-path>"
+#   exit 1
+# fi
 
-# Second argument is the CUCO source path
-CUCO_SRC_PATH="$2"
-if [ -z "$CUCO_SRC_PATH" ]; then
-  echo "Usage: $0 <sql-plan-compiler-dir> <cuco-src-path>"
+# # Second argument is the CUCO source path
+# CUCO_SRC_PATH="$2"
+# if [ -z "$CUCO_SRC_PATH" ]; then
+#   echo "Usage: $0 <sql-plan-compiler-dir> <cuco-src-path>"
+#   exit 1
+# fi
+
+SCALE_FACTOR=$1
+if [ -z "$SCALE_FACTOR" ]; then
+  echo "Usage: $0 <scale_factor>"
   exit 1
 fi
 
@@ -32,27 +39,26 @@ BUILD_DIR="$REPO_DIR/build/$BUILD_NAME"
 
 # Set the data directory if not already set
 if [ -z "$TPCH_DATA_DIR" ]; then
-  TPCH_DATA_DIR="$REPO_DIR/resources/data/tpch-1"
+  TPCH_DATA_DIR="$REPO_DIR/resources/data/tpch-$SCALE_FACTOR"
 fi
 
 # List of queries to run - 1, 3, 5, 6, 7, 8, 9
-QUERIES=(1 3 5 6 7 9 13)
+QUERIES=(1 3 4 5 6 7 8 9 10 12 13 14 16 17 18 19 20)
 
 # Iterate over the queries
 for QUERY in "${QUERIES[@]}"; do
-  CD_CMD="cd $SQL_PLAN_COMPILER_DIR/gpu-db/tpch"
+  CD_CMD="cd $SQL_PLAN_COMPILER_DIR/gpu-db/tpch-$SCALE_FACTOR"
   echo $CD_CMD
   $CD_CMD
 
   REPORT_BASE_FOLDER="$SQL_PLAN_COMPILER_DIR/reports/ncu"
-  CUR_GPU="4060" # TODO: Change this when you change GPUs
-  REPORT_FOLDER="$REPORT_BASE_FOLDER/$CUR_GPU/tpch-1" # TODO: Get this from the data-dir
+  REPORT_FOLDER="$REPORT_BASE_FOLDER/$CUR_GPU/tpch-$SCALE_FACTOR" # TODO: Get this from the data-dir
 
   MAKE_REPORT_FOLDER="mkdir -p $REPORT_FOLDER"
   echo $MAKE_REPORT_FOLDER
   $MAKE_REPORT_FOLDER
 
-  RUN_PROFILE_CMD="ncu --set full -f --export $REPORT_FOLDER/q$QUERY-tpch.ncu-rep ./build/dbruntime --data_dir $TPCH_DATA_DIR/ --query_num $QUERY"
+  RUN_PROFILE_CMD="ncu --set full -f --export $REPORT_FOLDER/q$QUERY-tpch-$SCALE_FACTOR.ncu-rep ./build/dbruntime --data_dir $TPCH_DATA_DIR/ --query_num $QUERY"
   echo $RUN_PROFILE_CMD
   $RUN_PROFILE_CMD # > op | tee 2>&1
 
