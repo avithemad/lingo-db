6c7207dde27f3da17c80e0f78ef4a6eb5ec3295c
chethanr <chethan.vasishta@gmail.com>
2025-08-17 16:08:14 +0530

    Queries selevtive of scale factor.

--- test/cuda-gen/perf/run_tpch_crystal_perf_tests.sh
+++ test/cuda-gen/perf/run_tpch_crystal_perf_tests.sh
@@ -94,8 +94,14 @@ fi
 
 
 QUERIES=(1 3 4 5 6 7 8 9 10 12 13 14 16 17 18 19 20)
-if [ $SCALE_FACTOR -gt 10 ]; then
-  QUERIES=(1 3 4 5 6 7 8 10 12 13 14 16 17 18 19 20) # query 9 has issues with memory.
+if [ "$CUR_GPU" == "A6000" ]; then
+  if [ $SCALE_FACTOR -gt 10 ]; then
+    QUERIES=(1 3 4 5 6 7 8 10 12 13 14 16 17 18 19 20) # query 9 has issues with memory.
+  fi
+elif [ "$CUR_GPU" == "4090" ]; then
+  if [ $SCALE_FACTOR -gt 10 ]; then
+    QUERIES=(1 3 5 6 7 8 10 12 13 14 16 17 18 19 20) # query 4, 9 has issues with memory.
+  fi
 fi
 
 TPCH_CUDA_GEN_DIR="$SQL_PLAN_COMPILER_DIR/gpu-db/tpch-$SCALE_FACTOR"
